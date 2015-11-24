a86ff1e3be751083f5e47bbb7e1f27f0f3a84196
Waseem Ahmed <rehan@Rehanuddins-Mac.local>
2015-11-24 13:22:34 +0500

    Initial Commit

---
+++ FaisalBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaisalBank
+//
+//  Created by Waseem Ahmed on 24/11/2015.
+//  Copyright (c) 2015 Click9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
